edd9ef9375f2a2dcc819abfc84e851f680cd5820
MGomez97 <manuelgomez19010@gmail.com>
2024-08-12 21:11:09 -0600

    Update Asignatura.cs

--- Examen/Asignatura.cs
+++ Examen/Asignatura.cs
@@ -1,15 +1,49 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 
-public class Asignatura : Alumno, IAsignatura;
+public class Asignatura : Alumno, IAsignatura
 {
-  public int N1 { get; set; }
-  public int N2 { get; set; }
-  public int N3 { get; set; }
-  public string NomAsignatura { get; set; }
-  public string Horario { get; set; }
-  public string NomDecente { get; set; }
+    public int N1 { get; set; }
+    public int N2 { get; set; }
+    public int N3 { get; set; }
+    public string NomAsignatura { get; set; }
+    public string Horario { get; set; }
+    public string NomDecente { get; set; }
 
-  public Asignatura (string Name, string Numcuenta, string Email, int N1, int N2, int N3, string NombreAsignatura, string Horario.
-  string NombreDecente)
+    public Asignatura(string Nombre, string NumeroCuenta, string Email, int n1, int n2, int n3, string NombreAsignatura, string horario,
+    string NombreDecente)
 
+    {
+        Name = Nombre;
+        NumCuenta = NumeroCuenta;
+        Correo = Email;
+        N1 = n1;
+        N2 = n2;
+        N3 = n3;
+        NomAsignatura = NombreAsignatura;
+        Horario = horario;
+        NomDecente = NombreDecente;
+
+    }
+
+    public double CalculaNotaFinal()
+    {
+        return (N1 + N2 + N3) / 3.0;
+    }
+
+    public string MensajeNotaFinal(double notaFinal)
+    {
+        if (notaFinal < 60)
+            return "Reprobado";
+        else if (notaFinal < 80)
+            return "Bueno";
+        else if (notaFinal < 90)
+            return "Muy Bueno";
+        else
+            return "Sobresaliente";
+    }
+
+    public void Imprimier()
+    { 
+    
+    }
 }
