3d980a0655cb670d205e1a6e0099495afa862193
MGomez97 <manuelgomez19010@gmail.com>
2024-08-12 21:27:31 -0600

    Update Asignatura.cs

--- Examen/Asignatura.cs
+++ Examen/Asignatura.cs
@@ -44,6 +44,25 @@ public class Asignatura : Alumno, IAsignatura
 
     public void Imprimier()
     { 
-    
+    base.Imrimir();
+
+        double notaFinal1 = CalculaNotaFinal();
+        double notaFinal2 = CalculaNotaFinalParametros(N1,N2,N3);
+
+        string Mensaje1 = MensajeNotaFinal(notaFinal1);
+        string Mensaje2 = MensajeNotaFinal(notaFinal2);
+
+        Console.WriteLine("Datos Sobre la Asignatura");
+        Console.WriteLine($"Nombre: {NomAsignatura}");
+        Console.WriteLine($"Horario: {Horario}");
+        Console.WriteLine($"Docente: {NomDecente}");
+
+        Console.WriteLine($"Nota Final 1: {notaFinal1} - {Mensaje1}");
+        Console.WriteLine($"Nota Final 2: {notaFinal2} - {Mensaje2}");
+    }
+
+        private double CalculaNotaFinalParametros(int n1,int n2, int n3)
+    {
+        return (n1 + n2 + n3) / 3.0;
     }
 }
