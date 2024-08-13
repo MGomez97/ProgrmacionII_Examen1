a5da9f27fbcff8c79e5bee8dc28699564d92256b
MGomez97 <manuelgomez19010@gmail.com>
2024-08-12 20:09:18 -0600

    Create .gitignore

--- Examen/obj/Debug/net8.0/Examen.AssemblyInfo.cs
+++ Examen/obj/Debug/net8.0/Examen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc13e4fbf5d631bc43b6dfe4a1009ce039168c53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
