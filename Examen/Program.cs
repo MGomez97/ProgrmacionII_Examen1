7bd73057729c4ca49dc345db55657d97c927ee89
MGomez97 <manuelgomez19010@gmail.com>
2024-08-12 21:53:02 -0600

    Update Program.cs

--- Examen/Program.cs
+++ Examen/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Progam
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+
+        }
+    }
+
+}
