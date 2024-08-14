585a2f9048012f862a9d0e0709e8efc5330479f3
MGomez97 <manuelgomez19010@gmail.com>
2024-08-13 18:36:02 -0600

    Update Program.cs

--- Examen/Program.cs
+++ Examen/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata;
 
 class Progam
 {
@@ -10,7 +12,17 @@ class Progam
             string nombre = Console.ReadLine();
 
             Console.WriteLine("Ingrese Numero de cuenta del alumno");
-        
+            string numcuenta = Console.ReadLine();
+
+            Console.WriteLine("Ingrese Correo del Alumno");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Ingrese nota del primer parcial: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 < 0 || n1 > 30)
+                throw new Exception("");
+
         }
     }
 
